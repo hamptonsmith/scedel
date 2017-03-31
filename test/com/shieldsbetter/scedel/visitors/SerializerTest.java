57ddb379e9fe6f85dbf020767d8c263796b24680
Hampton Smith <hampton.w.smith@gmail.com>
2017-03-31 01:36:13 -0400

    Added a serialization test for tokens.

--- test/com/shieldsbetter/scedel/visitors/SerializerTest.java
+++ test/com/shieldsbetter/scedel/visitors/SerializerTest.java
@@ -4,6 +4,7 @@ import com.shieldsbetter.scedel.ExecutionException;
 import com.shieldsbetter.scedel.Scedel;
 import com.shieldsbetter.scedel.ScedelTest;
 import com.shieldsbetter.scedel.StaticCodeException;
+import com.shieldsbetter.scedel.values.VToken;
 import com.shieldsbetter.scedel.values.Value;
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -33,6 +34,26 @@ public class SerializerTest {
         for (String ss : w.getBuffer().toString().split("\\$\\$")) {
             System.out.println(ss + "$$");
         }
+        
+        Value v2 = Serializer.deserialize(new StringReader(w.toString()));
+        
+        StringBuilder b1 = new StringBuilder();
+        v1.prettyRender(4, 0, b1);
+        
+        StringBuilder b2 = new StringBuilder();
+        v2.prettyRender(4, 0, b2);
+        
+        Assert.assertEquals(b1.toString(), b2.toString());
+    }
+    
+    @Test
+    public void metaTest() throws IOException {
+        VToken v1 = new VToken("abc");
+        
+        StringWriter w = new StringWriter();
+        Serializer.serialize(v1, new PrintWriter(w));
+        
+        System.out.println(w.getBuffer());
         
         Value v2 = Serializer.deserialize(new StringReader(w.toString()));
         
